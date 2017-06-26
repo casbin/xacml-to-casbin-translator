1324a618ea25a3c288fa3ef46fe04ae13ebe064f
Yang Luo <hsluoyz@qq.com>
2017-06-26 14:40:09 +0800

    Add the cases.

--- main.go
+++ main.go
@@ -1,6 +1,38 @@
 package main
 
+import "fmt"
+
+var Cases []string
+
+func InitCases() {
+	for i := 1; i < 2; i++ {
+		Cases = append(Cases, fmt.Sprintf("IIA%03d", i))
+	}
+}
+
+func PrintCases() {
+	fmt.Println("Cases:")
+
+	for i, c := range Cases {
+		fmt.Print(c)
+		if i != len(Cases) - 1 {
+			fmt.Print(", ")
+		}
+	}
+
+	fmt.Print("\n\n")
+}
+
 func main() {
-	p := ParsePolicy("ConformanceTests/IIA001Policy.xml")
-	PrintPolicy(p)
+	InitCases()
+	PrintCases()
+
+	//p := ParsePolicy("ConformanceTests/IIA001Policy.xml")
+	//PrintPolicy(p)
+
+	for _, c := range Cases {
+		r := ParseRequest(fmt.Sprintf("ConformanceTests/%sRequest.xml", c))
+		fmt.Print(c + ", ")
+		PrintRequest(r)
+	}
 }
