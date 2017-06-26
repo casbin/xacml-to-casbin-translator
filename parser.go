a56c10b0810a95a7c054ba9c9e609b227ad1d3a0
Yang Luo <hsluoyz@qq.com>
2017-06-26 14:37:16 +0800

    Parse the request.

--- parser.go
+++ parser.go
@@ -31,5 +31,60 @@ func ParsePolicy(path string) *pdp.Policy {
 
 func PrintPolicy(p *pdp.Policy) {
 	// fmt.Printf("%+v\n", p)
-	fmt.Print(p.Rules[0].Target.Subjects.Subjects[0].SubjectMatch.AttributeValue.Value)
+
+	sub := ""
+	for i, subject := range p.Rules[0].Target.Subjects.Subjects {
+		sub += subject.SubjectMatch.AttributeValue.Value
+		if i != len(p.Rules[0].Target.Subjects.Subjects) - 1 {
+			sub += ", "
+		}
+	}
+	fmt.Println(sub)
+
+	obj := ""
+	for i, object := range p.Rules[0].Target.Resources.Resources {
+		obj += object.ResourceMatch.AttributeValue.Value
+		if i != len(p.Rules[0].Target.Resources.Resources) - 1 {
+			obj += ", "
+		}
+	}
+	fmt.Println(obj)
+
+	act := ""
+	for i, subject := range p.Rules[0].Target.Actions.Actions {
+		act += subject.ActionMatch.AttributeValue.Value
+		if i != len(p.Rules[0].Target.Actions.Actions) - 1 {
+			act += ", "
+		}
+	}
+	fmt.Println(act)
+}
+
+func ParseRequest(path string) *Request {
+	xmlFile, err := os.Open(path)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return nil
+	}
+	defer xmlFile.Close()
+
+	var p Request
+	s, err := xmlFile.Stat()
+	if err != nil {
+		fmt.Println("Error stating file:", err)
+		return nil
+	}
+	size := s.Size()
+	b := make([]byte, size)
+	xmlFile.Read(b)
+	err = xml.Unmarshal([]byte(b), &p)
+	return &p
+}
+
+func PrintRequest(r *Request) {
+	sub := r.Subject.Attribute.AttributeValue
+	obj := r.Resource.Attribute.AttributeValue
+	act := r.Action.Attribute.AttributeValue
+
+	fmt.Println(sub + ", " + obj + ", " + act)
 }
---
+++ request.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/xml"
+)
+
+type Request struct {
+	Subject                Subject        `xml:"Subject"`
+	Resource               Resource       `xml:"Resource"`
+	Action                 Action         `xml:"Action"`
+}
+
+type Subject struct {
+	XMLName                xml.Name       `xml:"Subject"`
+	Attribute              Attribute      `xml:"Attribute"`
+}
+
+type Resource struct {
+	XMLName                xml.Name       `xml:"Resource"`
+	Attribute              Attribute      `xml:"Attribute"`
+}
+
+type Action struct {
+	XMLName                xml.Name       `xml:"Action"`
+	Attribute              Attribute      `xml:"Attribute"`
+}
+
+type Attribute struct {
+	XMLName                xml.Name       `xml:"Attribute"`
+	AttributeValue         string         `xml:"AttributeValue"`
+}
